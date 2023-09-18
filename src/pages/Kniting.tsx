80a03a1c41fcb4045b16ee4c8a98ba3f10026961
aurorakennedy <aurorke@stud.ntnu.no>
2023-09-18 21:46:00 +0200

    strikke side oppdatert innhold, plus navbar

--- src/pages/Kniting.tsx
+++ src/pages/Kniting.tsx
@@ -1,36 +1,26 @@
+import Navbar from "../components/Navbar";
+
 const Kniting = () => {
   return (
     <>
       <body>
         {/* Bakgrunn */}
-        <main className="bg-blue-400">
-          <div className="text-center pt-10">
-            <h1 className=" text-3xl text-fuchsia-950">
-              Jeg elsker å strikke, så dette er en del av mine prosjekter
+        <main className="bg-blue-200">
+          <Navbar />
+
+          <div className="text-center pt-20">
+            <h1 className=" text-2xl text-black font-mono">
+              Dette er noen av min strikke prosjekter:
             </h1>
-            <p>Men først litt kjærlighet til mams som lærte meg å strikke </p>
           </div>
 
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-32">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-32 font-mono">
             <div className=" text-center">
               <img
                 className="h-auto max-w-full rounded-lg"
-                src="/images/strikk/føyka.jpeg"
+                src="/images/strikk/zipJakke.jpeg"
                 alt=""
               ></img>
-              <span className=" text-gray-800">
-                Føyka genser <br /> Oppskrift av Linka Neumann{" "}
-              </span>
-            </div>
-            <div className=" text-center">
-              <img
-                className="h-auto max-w-full rounded-lg"
-                src="/images/strikk/ulven.jpeg"
-                alt=""
-              ></img>
-              <span className=" text-gray-800">
-                Svartulv genser <br /> Oppskrift av Linka Neumann{" "}
-              </span>
             </div>
             <div className=" text-center">
               <img
@@ -52,10 +42,24 @@ const Kniting = () => {
             <div className=" text-center">
               <img
                 className="h-auto max-w-full rounded-lg"
-                src="/images/strikk/zipJakke.jpeg"
+                src="/images/strikk/ulven.jpeg"
                 alt=""
               ></img>
+              <span className=" text-gray-800">
+                Svartulv genser <br /> Oppskrift av Linka Neumann{" "}
+              </span>
             </div>
+            <div className=" text-center">
+              <img
+                className="h-auto max-w-full rounded-lg"
+                src="/images/strikk/føyka.jpeg"
+                alt=""
+              ></img>
+              <span className=" text-gray-800">
+                Føyka genser <br /> Oppskrift av Linka Neumann{" "}
+              </span>
+            </div>
+
             <div className=" text-center">
               <img
                 className="h-auto max-w-full rounded-lg"
